5ff18354038e3442c8b1b11bdf83b53cc81d836d
Ashu jain <jarrypx@gmail.com>
2026-07-18 15:13:15 +0530

    Refactor main.tsx to include routing with React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.tsx'
+import Admin from './Admin.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Main Website */}
+        <Route path="/" element={<App />} />
+        
+        {/* Secret Admin Panel */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
